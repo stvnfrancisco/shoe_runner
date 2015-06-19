a65c0f08146a05f492af06310149109280e0096a
steven francisco <stvnfrancisco@yahoo.com>
2015-06-19 15:16:20 -0700

    add shoe_runner_integration_spec.rb to spec folder. Specs pass in rspec.

---
+++ spec/shoe_runner_integration_spec.rb
@@ -0,0 +1,17 @@
+require("spec_helper")
+
+describe('the add shoes process', {:type => :feature}) do
+  it('allows the user to add shoes to the database') do
+    visit('/')
+    fill_in('Add a shoe', :with => 'vans')
+    click_button('Save Shoe')
+    expect(page).to have_content('Vans')
+  end
+
+  it('allows the user to add stores to the database') do
+    visit('/')
+    fill_in('Add Store', :with => 'Zummies')
+    click_button('Save Store')
+    expect(page).to have_content('Zummies')
+  end
+end
