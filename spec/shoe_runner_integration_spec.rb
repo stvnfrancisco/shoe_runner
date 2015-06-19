0404216ace22d1d8f46bd5213f7522b51bc9872b
steven francisco <stvnfrancisco@yahoo.com>
2015-06-19 16:13:52 -0700

    text edits... size changes. add route to store list from index page.

--- spec/shoe_runner_integration_spec.rb
+++ spec/shoe_runner_integration_spec.rb
@@ -3,7 +3,7 @@ require("spec_helper")
 describe('the add shoes process', {:type => :feature}) do
   it('allows the user to add shoes to the database') do
     visit('/')
-    fill_in('Add a shoe', :with => 'vans')
+    fill_in('Add Shoe', :with => 'vans')
     click_button('Save Shoe')
     expect(page).to have_content('Vans')
   end
